e8fb172502a203063a78fe19d20b7c697afbe557
Kaitlyn Park <juyoung.toronto@gmail.com>
2023-03-20 13:54:46 -0400

    added profile image button that signs the user out on click in the side nav bar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,6 +1,12 @@
-import NewChat from "./NewChat"
+'use client'
+
+import { useSession, signOut } from "next-auth/react";
+import Image from "next/image"
+import NewChat from "@/app/components/NewChat"
 
 function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -10,6 +16,15 @@ function Sidebar() {
         </div>
         {/* Map through the ChatRows */}
       </div>
+      {session &&
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          onClick={() => signOut()}
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+          src={session.user?.image! || ''}
+          alt="Profile pic"
+        />
+      }
     </div>
   )
 }
